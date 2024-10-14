c103657b0cd6327659ee437fef72443bad95f8cf
melxiory <sibagatullinnail@gmail.com>
2024-10-14 21:46:51 +0300

    update async_scraper_unsplash_com.py

--- async_scraper_unsplash_com.py
+++ async_scraper_unsplash_com.py
@@ -12,16 +12,18 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def scraping(driver, wait):
+  # изначально на странице 20 фото, после нажатия 40 фото
   button = wait.until(EC.presence_of_element_located((By.XPATH, '//button[text()="Load more"]')))
   button.click()
   time.sleep(1)
+  # каждый цикл увеличивает количество фото на 20
   for _ in range(1):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     time.sleep(1)
-  # select the node images on the page
+  # ищем все кнопки для скачивания оригинального изображения
   image_html_nodes = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[data-testid=\"non-sponsored-photo-download-button\"]")))
-  # where to store the scraped image url
   image_urls = []
+  # вытаскиваем URL для скачивания
   for image_html_node in image_html_nodes:
     try:
       image_url = image_html_node.get_attribute("href")
@@ -41,19 +43,22 @@ async def download_pictures(image_url, number, session, semaphore):
 
 
 async def main():
-  # initialize a Chrome WebDriver instance
+  # Инициализация драйвера Хрома
   driver = webdriver.Chrome()
-  # to avoid issues with responsive content
   driver.maximize_window()
-  # the URL of the target page
-  url = "https://unsplash.com/s/photos/wallpaper?license=free"
-  # visit the target page in the controlled browser
+  # URL для скрапинга, можно подставить и другой "https://unsplash.com/s/photos/nature?license=free"
+  # главное чтобы структура сайта не менялась
+  url = "https://unsplash.com/s/photos/desktop-wallpapers?license=free"
+  # устанавливаем таймаут для ожидания загрузки html объектов
   wait = WebDriverWait(driver, 10)
   driver.get(url)
   image_urls = scraping(driver, wait)
+  # удаление и создание пустой папки
   shutil.rmtree('./images', ignore_errors=True)
   os.mkdir('images')
+  # устанавливаем семафор чтобы не перегружать сервер
   semaphore = asyncio.Semaphore(5)
+  # запускаем сессию и задачу на скачивание на каждый URL
   async with aiohttp.ClientSession() as session:
     downloaders = [asyncio.create_task(download_pictures(image_url, number, session, semaphore)) for number, image_url in enumerate(image_urls)]
     await asyncio.gather(*downloaders)
